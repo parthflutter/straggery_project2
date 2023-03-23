b4cf313b3a16373876819cba95fa135ad354cc03
user <savaliyaparth712@gmail.com>
2023-03-23 17:29:11 +0530

    hello

--- lib/screen/staggered_app/provider/staggered_provider.dart
+++ lib/screen/staggered_app/provider/staggered_provider.dart
@@ -8,6 +8,13 @@ class Home_provider extends ChangeNotifier{
     "assets/images/3.jpg",
     "assets/images/4.jpg",
     "assets/images/5.jpg",
+  ];
+  List name=[
+    "Images",
+    "Images",
+    "Images",
+    "Images",
+    "Images",
 
   ];
 }
--- lib/screen/staggered_app/view/staggered_screen.dart
+++ lib/screen/staggered_app/view/staggered_screen.dart
@@ -9,50 +9,65 @@ class staggered_screen extends StatefulWidget {
   @override
   State<staggered_screen> createState() => _staggered_screenState();
 }
-Home_provider?home_providertrue,home_providerfalse;
+
+Home_provider? home_providertrue, home_providerfalse;
+
 class _staggered_screenState extends State<staggered_screen> {
   @override
   Widget build(BuildContext context) {
-    home_providerfalse=Provider.of<Home_provider>(context,listen: false);
-    home_providertrue=Provider.of<Home_provider>(context,listen: true);
+    home_providerfalse = Provider.of<Home_provider>(context, listen: false);
+    home_providertrue = Provider.of<Home_provider>(context, listen: true);
     return SafeArea(
         child: Scaffold(
           backgroundColor: Colors.black,
-      appBar: AppBar(
-        title: Text("Straged Grid View"),
-        centerTitle: true,
-      ),
-      body: GridView.custom(
-        gridDelegate: SliverWovenGridDelegate.count(
-          crossAxisCount: 2,
-          mainAxisSpacing: 8,
-          crossAxisSpacing: 8,
-          pattern: [
-            WovenGridTile(1),
-            WovenGridTile(
-              5 / 7,
-              crossAxisRatio: 0.9,
-              alignment: AlignmentDirectional.centerEnd,
+          appBar: AppBar(
+            backgroundColor: Colors.black,
+            title: Text("Straged Grid View"),
+            centerTitle: true,
+          ),
+          body: GridView.custom(
+            gridDelegate: SliverWovenGridDelegate.count(
+              crossAxisCount: 2,
+              mainAxisSpacing: 8,
+              crossAxisSpacing: 8,
+              pattern: [
+                WovenGridTile(1),
+                WovenGridTile(
+                  5 / 7,
+                  crossAxisRatio: 0.9,
+                  alignment: AlignmentDirectional.centerEnd,
+                ),
+              ],
             ),
-          ],
-        ),
-        childrenDelegate: SliverChildBuilderDelegate(
-          childCount: 5,
-          (context, index) => Tile(index: index),
-        ),
-      ),
-    ),
-    );
+            childrenDelegate: SliverChildBuilderDelegate(
+              childCount: 5,
+                  (context, index) => Tile(index: index),
+            ),
+          ),
+        ));
   }
 
   Tile({required int index}) {
     return Container(
-      height: 100,
-      width: 100,
       color: Colors.black38,
-      child: Image.asset(
-        "${home_providerfalse!.Images[index]}",
-        fit: BoxFit.cover,
+      child: Stack(
+        children: [
+          Container(
+            height: double.infinity,
+            child: Image.asset(
+              "${home_providerfalse!.Images[index]}",
+              fit: BoxFit.cover,
+            ),
+          ),
+          Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                  color: Colors.white,
+                  height: 25,
+                  width:  double.infinity,
+                  alignment: Alignment.center,
+                  child: Text("${home_providerfalse!.name[index]}",style: TextStyle(fontSize: 18,color: Colors.black),))),
+        ],
       ),
     );
   }
